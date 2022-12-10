14c78a35e61e28a460077acf0a671191511934b7
silje <angelviksilje@gmail.com>
2022-12-10 03:14:38 +0100

    user register user login user update avatar user view credits user create listing *

--- src/main.js
+++ src/main.js
@@ -7,11 +7,9 @@ export const API_BASE_URL = 'https://api.noroff.dev/api/v1/auction';
 export const API_LISTINGS_URL = `${API_BASE_URL}/listings`;
 
 
-
 //export const bidsUrl = `${API_BASE_URL}/listings/<id>/bids`;
 //export const singleUrl = `${API_BASE_URL}/listings/`;
 //export const deleteUrl = `${API_BASE_URL}/listings/{}`;
-
 
 
 // BUTTONS
@@ -30,17 +28,23 @@ document.getElementById("focusButton").addEventListener("click", () => {
     console.log(`ITEMS: ${item.innerHTML}\n\nITEMS ID: ${item.id}`);
 });
 
-// if user is logged in, remove class "invisible"
+// if user is logged in
 if (localStorage.getItem("accessToken")) {
     document.getElementById("welcomeContainer").classList.remove("invisible");
+}
+
+// if user is not logged in
+if (!localStorage.getItem("accessToken")) {
+    [...document.querySelectorAll('.welcomeUser')].classList.remove("hidden");
 }
 
 // menu profile button - if you're not logged in, write alert and redirect to log in
 const profilePageButton = document.getElementById("profilePageButton");
 profilePageButton.addEventListener('click', () => {
-    if (!localStorage.getItem("accessToken")) {
-        window.alert("You need to be logged in to view your profile page");
-        window.location = '../login/index.html';
+    if (localStorage.getItem("accessToken")) {
+        window.location = '../profile/index.html';
     }
-    window.location = '../profile/index.html';
-})
+    window.alert("You need to be logged in to view your profile page");
+    window.location = '../login/index.html';
+});
+
