661d1a881d3fa167d12255d9d83957f4e19a62e3
silje <angelviksilje@gmail.com>
2022-12-10 03:44:59 +0100

    user register user login user update avatar user view credits user create listing *

--- src/create-listing.js
+++ src/create-listing.js
@@ -1,4 +1,13 @@
 import {API_LISTINGS_URL} from "./main";
+// Open Modal
+document.getElementById("modalOpenButton").addEventListener('click', () => {
+    document.getElementById("modal").classList.remove("hidden");
+})
+
+// Close Modal
+document.getElementById("modalCloseButton").addEventListener('click', () => {
+    document.getElementById("modal").classList.add("hidden");
+})
 
 let title = document.getElementById('createTitle').value;
 let media = document.getElementById('createMedia').value;
--- src/main.js
+++ src/main.js
@@ -48,13 +48,4 @@ profilePageButton.addEventListener('click', () => {
     window.location = '../profile/index.html';
 });
 
-// Open Modal
-document.getElementById("modalOpenButton").addEventListener('click', () => {
-    document.getElementById("modal").classList.remove("hidden");
-})
-
-// Close Modal
-document.getElementById("modalCloseButton").addEventListener('click', () => {
-    document.getElementById("modal").classList.add("hidden");
-})
 
